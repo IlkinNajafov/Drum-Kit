ffab5c74a51feeb9e864b33327ceacf825ae950d
IlkinNajafov <najafovilkinn@gmail.com>
2020-03-16 21:39:50 +0400

    Added features to JS

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+
+        function () {
+
+            alert("I got clicked!");
+        })
+};
+
